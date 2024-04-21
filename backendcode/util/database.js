87b5231d5d7f78ca7b58788d2c59f8470e4c3bf6
Nishmack <nishmayuzuf@gmail.com>
2024-04-21 13:38:35 +0530

    expense tracker

---
+++ backendcode/util/database.js
@@ -0,0 +1,8 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("node-complete", "root", "nishma@99", {
+  dialect: "mysql",
+  host: "localhost",
+});
+
+module.exports = sequelize;
